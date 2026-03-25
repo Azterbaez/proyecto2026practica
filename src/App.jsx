f90435d05fd4f179bba528293fb2b2ebc2e4080c
azterbaez <rivasmingito48@gmail.com>
2026-03-25 00:47:49 -0600

    estructura 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState} from 'react'
-import './App.css'
-const App = () => {
-return (
-      <>
-        <h1> Componente principal</h1>
-        </>
-)
+import { jsxDEV } from "react/jsx-dev-runtime";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Encabezado from "./assets/components/navegacion/Encabezado";
+import Inicio from "./assets/views/inicio";
+import Categorias from "./assets/views/Categorias";
+import Catalogo from "./assets/views/Catalogo";
+import Productos from "./assets/views/Productos";
+import Login from "./assets/views/Login";
+import RutaProtegida from "./assets/components/rutas/RutasProtegida";
+import Pagina404 from "./assets/views/Pagina404";
+import "./App.css";
 
+const App = () => {
+  return (
+    <Router>
+
+      <Encabezado />
+
+      <main className="margen-superior-main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </main>
+
+    </Router>
+  );
 }
+
 export default App;
